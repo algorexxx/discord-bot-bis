c322253efbb36769d454a934ac4537753f2c97b9
ernyx <47061893+ernyx@users.noreply.github.com>
2020-05-19 01:21:26 +0200

    Added db backup module and bug fixes.

---
+++ modules/backup.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+async function backup(db) {
+  const userData = db.get("users");
+  const ebData = db.get("eyebleach");
+  const funData = db.get("fun");
+  const hebData = db.get("hoteyebleach");
+  const songData = db.get("songs");
+
+  let data = [
+    { data: await userData.find({}), name: "users" },
+    { data: await ebData.find({}), name: "ebs" },
+    { data: await hebData.find({}), name: "hebs" },
+    { data: await funData.find({}), name: "funs" },
+    { data: await songData.find({}), name: "songs" },
+  ];
+
+  // stringify JSON Object
+  data.forEach(function (d) {
+    var jsonContent = JSON.stringify(d.data);
+
+    fs.writeFile(
+      "./olddb/backup" + d.name + ".json",
+      jsonContent,
+      "utf8",
+      function (err) {
+        if (err) {
+          console.log(
+            "An error occured while writing JSON Object to File.",
+            d.name
+          );
+          return console.log(err);
+        }
+
+        console.log(d.name + " JSON file has been saved.");
+      }
+    );
+  });
+
+  console.log("done");
+}
+
+module.exports = backup;
