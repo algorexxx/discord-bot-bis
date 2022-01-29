6f076b584f3585664dc338adc08130472794b5b7
Alg Ore <hyperionxxx@gmail.com>
2022-01-29 21:24:50 +0100

    Remove interaction commands

--- botv2.js
+++ botv2.js
@@ -22,29 +22,6 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-
-    const user = await getUser(interaction.user.id, userData);
-
-    switch (interaction.commandName) {
-        case "heb":
-            const isPg13Channel = interaction.channel == (await client.channels.fetch("434824496856301591"));
-            if (isPg13Channel) {
-                interaction.reply("Psst, not here.. ;)\n");
-                return;
-            }
-            interaction.reply(await heb(interaction.options.getString('imgurl'), user, db, client));
-            break;
-        case "rheb":
-            interaction.reply(await rheb(interaction.options.getString('imgid'), user, db));
-            break;
-        case "stats":
-            interaction.reply(await stats(interaction.options.getString('username'), user, db, client));
-            break;
-    }
-});
-
 client.on("messageCreate", async message => { 
     if (message.content.toLowerCase().includes("bitco")) {
         message.reply("https://www.youtube.com/watch?v=e5nyQmaq4k4");
